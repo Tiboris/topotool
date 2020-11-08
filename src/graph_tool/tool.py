8496ce34e472bc2e2ee3701aa6092c12e35f3d58
Tibor Dudlák <tdudlak@redhat.com>
2020-11-08 18:40:58 +0100

    Improve context loading add new command names
    Signed-off-by: Tibor Dudlák <tdudlak@redhat.com>

--- src/graph_tool/tool.py
+++ src/graph_tool/tool.py
@@ -12,6 +12,18 @@ from graph_tool.graphs import Graph
 
 GRAPH_DATA = "graph_input_data"
 GRAPH_OBJECT = "graph_object"
+GRAPH_NX = "graph_networkx"
+
+
+def load_context(ctx, storage):
+    if not ctx.obj:
+        ctx.ensure_object(dict)
+        with shelve.open(storage) as db:
+            ctx.obj[GRAPH_DATA] = db[GRAPH_DATA]
+            ctx.obj[GRAPH_OBJECT] = db[GRAPH_OBJECT]
+            ctx.obj[GRAPH_NX] = db[GRAPH_NX]
+
+    return ctx
 
 
 @click.group(chain=True)
@@ -45,6 +57,13 @@ def load(ctx, input_file, delimiter, weight_delimiter, storage):
     )
     ctx.obj[GRAPH_OBJECT] = graph_data
 
+    G = nx.Graph()
+    for vertex in graph_data.vertices:
+        G.add_node(vertex)
+
+    G.add_edges_from(graph_data.edge_list)
+    ctx.obj[GRAPH_NX] = G
+
     with shelve.open(storage) as db:
         for key in ctx.obj:
             db[key] = ctx.obj[key]
@@ -55,21 +74,35 @@ def load(ctx, input_file, delimiter, weight_delimiter, storage):
 @click.pass_context
 def draw(ctx, storage):
     """Draw a topology graph as picture."""
-    if not ctx.obj:
-        ctx.ensure_object(dict)
-        with shelve.open(storage) as db:
-            ctx.obj[GRAPH_DATA] = db[GRAPH_DATA]
-            ctx.obj[GRAPH_OBJECT] = db[GRAPH_OBJECT]
+    ctx = load_context(ctx, storage)
 
-    graph_data = ctx.obj[GRAPH_OBJECT]
-    G = nx.Graph()
-    for vertex in graph_data.vertices:
-        G.add_node(vertex)
-
-    G.add_edges_from(graph_data.edge_list)
-
+    G = ctx.obj[GRAPH_NX]
     nx.draw(G, with_labels=True, font_weight='bold')
     plt.show()
+
+
+@graphcli.command()
+@click.option("-o", "--output", default="./topology_playbook")
+@click.option("-s", "--storage", default="./.graph_storage.json")
+@click.pass_context
+def playbook(ctx, storage, output):
+    ctx = load_context(ctx, storage)
+
+    G = ctx.obj[GRAPH_NX]
+
+    levels = [["A"], list(G.adj["A"])]
+
+    print(levels)
+
+
+@graphcli.command()
+@click.option("-o", "--output", default="./graph_inventory")
+@click.option("-s", "--storage", default="./.graph_storage.json")
+@click.pass_context
+def inventory(ctx, output, storage):
+    ctx = load_context(ctx, storage)
+
+    print(output)
 
 
 if __name__ == "__main__":
